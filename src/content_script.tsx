ed3bdbde06ab9b34b0721004f01fb5957fcdd30c
Saito <kae.saito@digdir.no>
2023-06-19 11:02:48 +0200

    added span button to scan.

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,24 +1,21 @@
 chrome.runtime.onMessage.addListener(handleMessage);
- 
- 
- 
+
 function handleMessage(message: any, sender:any, sendResponse:any) {
   if (message.action === "getButtons") {
-    const buttons = Array.from(document.querySelectorAll("[role='button'], button, a, input[type='button'], input[type='submit']"));
+    const buttons = Array.from(document.querySelectorAll("[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"));
     sendResponse({ buttons });
   } else if (message.action === "highlightButtons") {
     const color = message.color;
-    const buttons = Array.from(document.querySelectorAll("[role='button'], button, a, input[type='button'], input[type='submit']")) as HTMLButtonElement[];
+    const buttons = Array.from(document.querySelectorAll("[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']")) as HTMLElement[];
     buttons.forEach((button) => {
-      button.style.border = "5px solid " + color;
-      //button.classList.add("button-highlight");
-      //console.log(button);
-      });
+      button.style.cssText += `border: 5px solid ${color} !important;`;
+    });
     sendResponse({ message: "Buttons highlighted" });
   } else if(message.color){
-    document.body.style.backgroundColor = message.color;
+    document.body.style.cssText += `background-color: ${message.color} !important;`;
     sendResponse({ message: "Background changed" });
   } else {
     sendResponse({ message: "Unknown action" });
   }
 }
+
