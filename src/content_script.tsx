14c5cfc487c46e686d0906eac0a48e6ef17cd87b
Saito <kae.saito@digdir.no>
2023-06-22 08:27:46 +0200

    change button frame design

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,21 +1,84 @@
+// chrome.runtime.onMessage.addListener(handleMessage);
+
+// function handleMessage(message: any, sender: any, sendResponse: any) {
+//   if (message.action === "getButtons") {
+//     const buttons = Array.from(
+//       document.querySelectorAll(
+//         "[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"
+//       )
+//     );
+//     sendResponse({ buttons });
+//   } else if (message.action === "highlightButtons") {
+//     const color = message.color;
+//     const buttons = Array.from(
+//       document.querySelectorAll(
+//         "[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"
+//       )
+//     ) as HTMLElement[];
+//     buttons.forEach((button) => {
+//       button.style.cssText += `background-color: ${color} !important;`;
+//     });
+//     sendResponse({ message: "Buttons highlighted" });
+//   } else if (message.action === "changeButtonsColor") {
+//     const color = message.color;
+//     const buttons = Array.from(
+//       document.querySelectorAll(
+//         "[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"
+//       )
+//     ) as HTMLElement[];
+//     buttons.forEach((button) => {
+//       button.style.backgroundColor = color;
+//     });
+//     sendResponse({ message: "Buttons color changed" });
+//   } else if (message.color) {
+//     document.body.style.cssText += `background-color: ${message.color} !important;`;
+//     sendResponse({ message: "Background changed" });
+//   } else {
+//     sendResponse({ message: "Unknown action" });
+//   }
+// }
+
+
 chrome.runtime.onMessage.addListener(handleMessage);
 
-function handleMessage(message: any, sender:any, sendResponse:any) {
+function handleMessage(message: any, sender: any, sendResponse: any) {
   if (message.action === "getButtons") {
-    const buttons = Array.from(document.querySelectorAll("[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"));
+    const buttons = Array.from(
+      document.querySelectorAll(
+        "[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"
+      )
+    );
     sendResponse({ buttons });
   } else if (message.action === "highlightButtons") {
     const color = message.color;
-    const buttons = Array.from(document.querySelectorAll("[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']")) as HTMLElement[];
+    const buttons = Array.from(
+      document.querySelectorAll(
+        "[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"
+      )
+    ) as HTMLElement[];
     buttons.forEach((button) => {
-      button.style.cssText += `border: 5px solid ${color} !important;`;
+      button.style.cssText += `
+        background-color: ${color} !important; 
+        border-radius: 10px;
+      
+      `;
     });
     sendResponse({ message: "Buttons highlighted" });
-  } else if(message.color){
+  } else if (message.action === "changeButtonsColor") {
+    const color = message.color;
+    const buttons = Array.from(
+      document.querySelectorAll(
+        "[role='button'], button, a, input[type='button'], input[type='submit'], span[role='button']"
+      )
+    ) as HTMLElement[];
+    buttons.forEach((button) => {
+      button.style.backgroundColor = color;
+    });
+    sendResponse({ message: "Buttons color changed" });
+  } else if (message.color) {
     document.body.style.cssText += `background-color: ${message.color} !important;`;
     sendResponse({ message: "Background changed" });
   } else {
     sendResponse({ message: "Unknown action" });
   }
 }
-
--- src/popup.tsx
+++ src/popup.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import Select from "react-select";
 import { createRoot } from "react-dom/client";
 import './style/popup.css';
 
 const Popup = () => {
   const [count, setCount] = useState(0);
   const [currentURL, setCurrentURL] = useState<string>();
+  const [selectedColor, setSelectedColor] = useState<string>(""); // Default color
+
 
   useEffect(() => {
     chrome.action.setBadgeText({ text: count.toString() });
@@ -51,49 +54,83 @@ const Popup = () => {
     });
   };
 
+  const changeButtonsColor = (color: string) => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+      const tab = tabs[0];
+      if (tab.id) {
+        chrome.tabs.sendMessage(
+          tab.id,
+          {
+            action: "changeButtonsColor",
+            color: color,
+          },
+          (msg) => {
+            console.log("result message:", msg);
+          }
+        );
+      }
+    });
+  };
+
   const getButtons = () => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
 
       if (activeTab?.id) {
         chrome.tabs.sendMessage(
-          activeTab.id, {
-          action: "getButtons" 
-        }, (response) => {
-
-          if (response && response.buttons) {
-
-            console.log(response.buttons);
-          }else{
-            console.log(response);
-
+          activeTab.id,
+          {
+            action: "getButtons",
+          },
+          (response) => {
+            if (response && response.buttons) {
+              console.log(response.buttons);
+            } else {
+              console.log(response);
+            }
           }
-        });
+        );
       }
     });
   };
+
+  const colorOptions = [
+    { value: "", label: "Select Color" }, // Default title option
+    { value: "red", label: "Red" },
+    { value: "yellow", label: "Yellow" },
+    { value: "blue", label: "Blue" },
+    { value: "green", label: "Green" },
+    { value: "orange", label: "Orange" },
+    { value: "pink", label: "Pink" },
+  ];
 
   return (
     <>
       <div className={"content"}>
         <p>Current URL: {currentURL}</p>
-        <p>Current Time: <p className={"pink bold"}> {new Date().toLocaleTimeString()} </p> </p>
+        <p>
+          Current Time:{" "}
+          <p className={"pink bold"}> {new Date().toLocaleTimeString()} </p>{" "}
+        </p>
       </div>
 
       <div className={"bottom"}>
-        <button onClick={() => setCount(count + 1)}>
-          count up
-        </button>
+        <button onClick={() => setCount(count + 1)}>count up</button>
 
-        <button onClick={changeBackground}>
-          change background
-        </button>
+        <button onClick={changeBackground}>change background</button>
 
-        <button onClick={hightlightButtons}>
-          Ny funksjon
+        <button onClick={hightlightButtons}>Ny funksjon</button>
+
+        <Select
+          options={colorOptions}
+          value={colorOptions.find((option) => option.value === selectedColor)}
+          onChange={(selectedOption) => setSelectedColor(selectedOption?.value || "")}
+        />
+
+        <button onClick={() => changeButtonsColor(selectedColor)}>
+          Change color for the buttons
         </button>
       </div>
-
     </>
   );
 };
@@ -105,3 +142,11 @@ root.render(
     <Popup />
   </React.StrictMode>
 );
+
+
+
+
+
+
+
+
