5e7d2a87048618368acdb9fe35d2771ddb61e861
Nitschke <andreas.conradi.nitschke@digdir.no>
2023-06-23 12:46:46 +0200

    Added a layout to the sidebar with some example data, also edited manifest to add sidepanel support

---
+++ src/Sidebar/Buttons.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ToggleButtonProps, RegularButtonProps, CollapsibleArrowProps } from "./Interfaces";
+
+export const ToggleButton: React.FC<ToggleButtonProps> = ({ isChecked, onToggle }) => {
+  return (
+    <button className={`toggle-button ${isChecked ? 'pressed' : ''}`} onClick={(e) => { e.stopPropagation(); onToggle(); }}>
+      {isChecked ? 'Highlight' : 'Highlight'}
+    </button>
+  );
+};
+
+export const RegularButton: React.FC<RegularButtonProps> = ({ onClick }) => {
+  return (
+    <button className='regular-button' onClick={(e) => { e.stopPropagation(); onClick(); }}>
+      Jump to
+    </button>
+  );
+};
+
+export const CollapsibleArrowButton: React.FC<CollapsibleArrowProps> = ({ isExpanded }) => {
+  return <span>{isExpanded ? '↓' : '→'}</span>;
+};
+
