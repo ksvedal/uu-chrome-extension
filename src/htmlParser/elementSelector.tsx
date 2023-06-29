c99bca586a2d47e67196d885f703550254f025e6
Nitschke <andreas.conradi.nitschke@digdir.no>
2023-06-29 11:05:42 +0200

    added elementSelector to easily expand wich elements we want to find

---
+++ src/htmlParser/elementSelector.tsx
@@ -0,0 +1,32 @@
+export abstract class ElementSelector {
+    abstract selector: string;
+    getElements(): NodeListOf<HTMLElement> {
+        return document.querySelectorAll(this.selector) as NodeListOf<HTMLElement>;
+    }
+}
+
+/* export class ButtonSelector extends ElementSelector {
+    selector = "button, input[type='submit'], input[type='button'], [role='button']";
+} */
+//Example of a buttons selector that doesnt overlap with menu items
+ export class ButtonSelector extends ElementSelector {
+    selector = "button:not([role='menuitem']):not([role='menuitemcheckbox']):not([role='menuitemradio']), input[type='submit']:not([role='menuitem']):not([role='menuitemcheckbox']):not([role='menuitemradio']), input[type='button']:not([role='menuitem']):not([role='menuitemcheckbox']):not([role='menuitemradio']), [role='button']:not([role='menuitem']):not([role='menuitemcheckbox']):not([role='menuitemradio'])";
+}
+ 
+export class ImageSelector extends ElementSelector {
+    selector = "img:not([role='button'])";
+}
+
+export class LinkSelector extends ElementSelector {
+    selector = "a:not([role='button'])";
+}
+
+ export class MenuItems extends ElementSelector {
+    selector = "[role='menuitem'], [role='menuitemcheckbox'], [role='menuitemradio']";
+}
+ 
+export class Headings extends ElementSelector {
+    selector = "h1, h2, h3, h4, h5, h6"
+}
+
+
