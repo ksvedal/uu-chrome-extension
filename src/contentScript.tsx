e1fa0221fb7f516e603129d24f9475f877eef3cb
Nitschke <andreas.conradi.nitschke@digdir.no>
2023-06-29 13:30:37 +0200

    removed auduns old code and saved it locally

--- src/contentScript.tsx
+++ src/contentScript.tsx
@@ -30,39 +30,6 @@ function handleMessage(message: Message, sender: any, sendResponse: any) {
     /*
    Compares different actions and calls the appropriate function
  */
-
-   /**
-    * checkButtonAltText is not currently used, keeping it for future use
-    */
-    /* if (message.action === "checkButtonsAltText") {
-      const buttonsSelector = "button, input[type='submit'], input[type='button'], [role='button']";
-      const buttons = Array.from(document.querySelectorAll(buttonsSelector)) as HTMLElement[];
-      let noAltTextCount = 0;
-
-      buttons.forEach((button) => {
-        let altText = "";
-        if (button.tagName.toLowerCase() === "img") {
-          altText = button.getAttribute("alt") as string;
-        } else {
-          altText = button.innerText.trim();
-        }
-        if (!altText) {
-          noAltTextCount++;
-          console.log("Button does not have alternative text:", button);
-          button.style.cssText += `border: 5px solid #FF0000 !important;`;
-        } else {
-          button.style.cssText += `border: 5px solid #0000FF !important;`
-          // Handle the button with alternative text
-        }
-      });
-      console.log("Antall knapper uten alt tekst:", noAltTextCount);
-      chrome.storage.local.set({ noAltTextCount: noAltTextCount } as { noAltTextCount: number },
-        () => {
-          chrome.runtime.sendMessage({ action: "noAltTextCountUpdated", count: noAltTextCount })
-        })
-      sendResponse({ message: "Button alternative text checked" });
-
-    } else */
       if (message.action === "scanPage") {
         let result: ElementType[] = _scan.scanPage();
         sendResponse(result);
