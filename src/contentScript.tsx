9b5b490e56a100d8eae4e62d736b4dece93cbebd
Nitschke <andreas.conradi.nitschke@digdir.no>
2023-06-29 10:11:47 +0200

    fixed button interaction with highlighting and highlightAll. Also moved CollapsibleItemElement into CollapsibleItemType for easier interaciton between the two.

--- src/contentScript.tsx
+++ src/contentScript.tsx
@@ -1,4 +1,4 @@
-import { HighlightAllMessage, HighlightMessage, Message, ScanPageMessage } from './messageObjects/message';
+import { HighlightAllMessage, HighlightAndRemovePreviousMessage, HighlightMessage, Message, ScanPageMessage, UnhighlightAllAndHighlightSingleMessage } from './messageObjects/message';
 import { PageInteractor } from './htmlParser/pageInteractor';
 import { WebsiteScanner } from './htmlParser/websiteScanner';
 import { ElementType } from './sidebar/interfaces';
@@ -22,52 +22,41 @@ document.head.append(styleElement);
  * @param sendResponse 
  */
 function handleMessage(message: Message, sender: any, sendResponse: any) {
-  const _page: PageInteractor = new PageInteractor();
-  const _scan: WebsiteScanner = new WebsiteScanner();
+  try {
+    const _page: PageInteractor = new PageInteractor();
+    const _scan: WebsiteScanner = new WebsiteScanner();
+    console.log("Message received:", message.action);
 
-  /*
-    Compares different actions and calls the appropriate function
-  */
-  if (message.action === "checkButtonsAltText") {
-    const buttonsSelector = "button, input[type='submit'], input[type='button'], [role='button']";
-    const buttons = Array.from(document.querySelectorAll(buttonsSelector)) as HTMLElement[];
-    let noAltTextCount = 0;
-
-    buttons.forEach((button) => {
-      let altText = "";
-      if (button.tagName.toLowerCase() === "img") {
-        altText = button.getAttribute("alt") as string;
-      } else {
-        altText = button.innerText.trim();
-      }
-      if (!altText) {
-        noAltTextCount++;
-        console.log("Button does not have alternative text:", button);
-        button.style.cssText += `border: 5px solid #FF0000 !important;`;
-      } else {
-        button.style.cssText += `border: 5px solid #0000FF !important;`
-        // Handle the button with alternative text
-      }
-    });
-    console.log("Antall knapper uten alt tekst:", noAltTextCount);
-    chrome.storage.local.set({ noAltTextCount: noAltTextCount } as { noAltTextCount: number },
-      () => {
-        chrome.runtime.sendMessage({ action: "noAltTextCountUpdated", count: noAltTextCount })
-      })
-    sendResponse({ message: "Button alternative text checked" });
-
-  }  else if (message.action === "scanPage") {
-    //Sends a list of ElementType containing all the elementObjects on the page
-    let result: ElementType[] = _scan.scanPage();
-    sendResponse(result);
-  } else if (message.action === "highlightElement") {
-    _page.handleHighlightSingle((message as HighlightMessage));
-    sendResponse({ message: "HighlightSingle response" });
-  } else if (message.action === "highlightAllElements") {
-    _page.highlightElements((message as HighlightAllMessage));
-    sendResponse({message: "Highlighted all with type"})
-  } else {
-    sendResponse({ message: "Unknown action" });
+    if (message.action === "scanPage") {
+      let result: ElementType[] = _scan.scanPage();
+      sendResponse(result);
+    } else if (message.action === "highlightElement") {
+      _page.handleHighlightSingle((message as HighlightMessage));
+      sendResponse({ message: "HighlightSingle response" });
+    } else if (message.action === "highlightAllElements") {
+      _page.highlightAllWithType((message as HighlightAllMessage));
+      sendResponse({ message: "Highlighted all with type" })
+    } else if (message.action === "highlightAndRemovePrevious") {
+      _page.highlightAndRemovePrevious((message as HighlightAndRemovePreviousMessage));
+      sendResponse({ message: "Highlighted and removed previous" })
+    } else if (message.action === "unhighlightAllAndHighlightSingle") {
+      _page.unhighlightAllAndHighlightSingle((message as UnhighlightAllAndHighlightSingleMessage));
+      sendResponse({ message: "Unhighlighted all and highlighted single" })
+    } else {
+      sendResponse({ message: "Unknown action" });
+    }
+    return true; //Keeps the message channel open
+  } catch (error: unknown) {
+    console.error(`Error handling message:`, error);
+    let errorMessage = '';
+    if (error instanceof Error) {
+      errorMessage = error.message;
+    } else {
+      errorMessage = String(error);
+    }
+    sendResponse({ message: `Error: ${errorMessage}` });
+    return true; //Keeps the message channel open
   }
-  return true; //Keeps the message channel open
+
 }
+
--- src/sidebar/sidebar.tsx
+++ src/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
-import React, { useContext, useState } from 'react';
-import { CollapsibleItemType, CollapsibleItemElement } from './collapsibleItem';
+import React, { useState } from 'react';
+import { CollapsibleItemType } from './collapsibleItem';
 import { createRoot } from "react-dom/client";
 import "../style/sidebar.css";
 import { ElementObject, ElementType } from "./interfaces";
@@ -11,10 +11,13 @@ import { WebsiteScanner } from '../htmlParser/websiteScanner';
 
 const Sidebar: React.FC = () => {
   const [scanPage, setScanPage] = useState<ElementType[]>([]); // initialize scanPage state as an empty array
-  const [highlightedElement, setHighlightedElement] = useState<ElementObject | null>(null);
   const [websiteURL, setWebsiteURL] = useState<string>("");
-  const _message : MessageSender = new MessageSender();
-  const _scan : WebsiteScanner = new WebsiteScanner();
+  const [isAllHighlighted, setIsAllHighlighted] = useState<boolean>(false); // add this line
+  const [currentHighlighted, setCurrentHighlighted] = useState<ElementObject | null>(null);
+
+
+  const _message: MessageSender = new MessageSender();
+  const _scan: WebsiteScanner = new WebsiteScanner();
 
 
   const fetchData = () => {
@@ -29,34 +32,26 @@ const Sidebar: React.FC = () => {
 
   return (
     <div className='App'>
-      <RegularButton text="Scan page" onClick={fetchData} /> 
-
+      <RegularButton text="Scan page" onClick={fetchData} />
       {/* This is the non-collapsible space */}
       <div style={{ height: '150px', backgroundColor: '#f5f5f5' }}>
         <TabIcon></TabIcon>
         <p>Site: {websiteURL} </p>
-        <button className='headline-button' onClick={(e) => { e.stopPropagation();}}>
+        <button className='headline-button' onClick={(e) => { e.stopPropagation(); }}>
           Åpen som tabell
         </button>
 
       </div>
 
       {/*for each element in ScanPage, creates a collapse menu with other nodes*/}
-      {scanPage.map((item, index) => 
-      <CollapsibleItemType key={index}
-      type={item}>
-
-        {/* For each node in the type, creates another collapse to display the button*/}
-        {item.nodes.map((item, index) =>
-          <CollapsibleItemElement 
-          key={index} 
-          object={item}
-          highlightedElement={highlightedElement}
-          setHighlightedElement={setHighlightedElement}>
-            {item.htmlString}
-          </CollapsibleItemElement>
-        )}
-      </CollapsibleItemType>)}
+      {scanPage.map((item, index) =>
+        <CollapsibleItemType key={index}
+          type={item}
+          setIsAllHighlighted={setIsAllHighlighted}
+          setCurrentHighlighted={setCurrentHighlighted}
+          isAllHighlighted={isAllHighlighted}
+        >
+        </CollapsibleItemType>)}
     </div>
   );
 }
