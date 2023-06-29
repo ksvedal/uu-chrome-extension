4cabbbf5535666a33aa9fa8d8540e15c2350bb8c
Saito <kae.saito@digdir.no>
2023-06-29 09:27:11 +0200

    Fixed to move to the tab

--- src/openSidepanel.js
+++ src/openSidepanel.js
@@ -13,3 +13,17 @@ chrome.tabs.onUpdated.addListener(async (tabId, info, tab) => {
     });
   
 });
+
+
+chrome.action.onClicked.addListener((tab) => {
+  const htmlFilePath = chrome.runtime.getURL('htmlTableBar.html');
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: openHtmlFileInNewTab,
+    args: [htmlFilePath]
+  });
+});
+
+function openHtmlFileInNewTab(htmlFilePath) {
+  chrome.tabs.create({ url: htmlFilePath });
+}
