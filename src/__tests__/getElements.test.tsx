db0c36451a21ac7e8d3fe572e690c0655764d44f
KRISTIAN BIRKELI <kristian.birkeli@gmail.com>
2023-07-12 15:08:49 +0200

    Working test for the ButtonSelcetor subclass and the getElements method. Need more test cases

--- src/__tests__/getElements.test.tsx
+++ src/__tests__/getElements.test.tsx
@@ -1,23 +1,51 @@
 import { ButtonSelector } from "../htmlParser/elementSelector";
 
-test('getElements finds the correct buttons', () => {
+describe('ButtonSelector', () => {
+    test('getElements should return the correct elements', () => {
+        // Create mock buttons that match the selector
+        const mockButtons: HTMLButtonElement[] = [
+        document.createElement('button'),
+        document.createElement('button'),
+        document.createElement('button'),
+        document.createElement('button'),
+        ];
 
-    // 1. Need an instance of the ButtonSelector
-    // 2. Want to use the 'getElements' method
-    // 3. Want the 'querySelectorAll' to find all correct elements in a predefined, known DOM
-    // 4. 
+        // Add unique attributes to differentiate buttons
+        mockButtons[0].id = 'button1';
+        mockButtons[1].classList.add('primary-button');
+        mockButtons[2].setAttribute('data-custom', 'button');
 
-    const mockDOMElements = document.createElement('div');
-    mockDOMElements.innerHTML = `
-        <button><button/>
-        <button role='menuitem'><button/>
-        <button role='button'><button/>
-    `
-    const querySelectorAllMock = jest.spyOn(mockDOMElements, 'querySelectorAll')
-   
+        // Create mock buttons that should not be matched by the selector
+        const nonMatchingButtons: HTMLButtonElement[] = [
+        document.createElement('button'),
+        document.createElement('button'),
+        ];
 
-    const testButtonSelector = new ButtonSelector()
+        // Add attributes to the non-matching buttons that should exclude them from the selector
+        nonMatchingButtons[0].setAttribute('role', 'menuitem');
+        nonMatchingButtons[1].setAttribute('disabled', 'true');
 
-    testButtonSelector.getElements()
+        // Create an instance of ButtonSelector
+        const buttonSelector: ButtonSelector = new ButtonSelector();
 
-})
+        // Mock the getElements method to return the mock buttons
+        jest.spyOn(buttonSelector, 'getElements').mockReturnValue(mockButtons as unknown as NodeListOf<HTMLElement>);
+
+        // Call the getElements method
+        const buttonElements: NodeListOf<HTMLElement> = buttonSelector.getElements();
+
+        // Assert the expected results
+        expect(buttonElements.length).toBe(3);
+        expect(buttonElements[0].id).toBe('button1');
+        expect(buttonElements[1].classList.contains('primary-button')).toBe(true);
+        expect(buttonElements[2].getAttribute('data-custom')).toBe('button');
+
+        // Assert that non-matching buttons are not included
+        nonMatchingButtons.forEach((button) => {
+        expect(buttonElements).not.toContain(button);
+        });
+
+        // Restore the mock
+        jest.restoreAllMocks();
+    });
+  });
