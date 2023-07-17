1899a0e4c2be19d085209242af00098701a45480
Eiril Ugulen <eiril.solveig.ugulen@digdir.no>
2023-07-17 13:25:59 +0200

    highlighElement test beginning

--- src/__tests__/contentScript.test.tsx
+++ src/__tests__/contentScript.test.tsx
@@ -8,28 +8,32 @@ import {
   Message,
   ScanPageMessage,
   UnhighlightAllAndHighlightSingleMessage,} from "../messageObjects/message";
-import { ElementType } from "../sidebar/interfaces";
+import { ElementObject, ElementType } from "../sidebar/interfaces";
+
 
 describe('Content Script', () => {
   let pageInteractor: PageInteractor;
   let websiteScanner: WebsiteScanner;
-  let messageSender: chrome.runtime.MessageSender;
+  let sender: chrome.runtime.MessageSender;
 
   beforeEach(() => {
     pageInteractor = new PageInteractor();
     websiteScanner = new WebsiteScanner();
-    messageSender = {} as chrome.runtime.MessageSender;
+    sender = {} as chrome.runtime.MessageSender;
   });
 
+
+  //CASE: scanPage
   test('handleMessage should scan the page', () => {
+    
     // Mock the `sendResponse` function
     const sendResponseMock = jest.fn();
 
     // Create a message object for the 'scanPage' action
-    const message: ScanPageMessage = { action: 'scanPage' };
+    const testMessage: ScanPageMessage = { action: 'scanPage' };
 
     // Call the handleMessage function
-    handleMessage(message, messageSender, sendResponseMock);
+    handleMessage(testMessage, sender, sendResponseMock);
 
     // Verify that sendResponse was called with an array (result of scanPage)
     expect(sendResponseMock).toHaveBeenCalledWith(expect.any(Array));
@@ -41,6 +45,53 @@ describe('Content Script', () => {
   });
 
 
+
+
+  // skal sjekke at case highlightElement bruker handleHighlighSingle på _page 
+  // der handleHighlighSingle i pageInteractor tar inn en message av typen HighlightMessage
+  // der klassen highlightMessage har et element: Elementobject og isChecked: boolean
+  // der ElementObject i interfaces har string, string, Elementresult, Elementattribute
+  // der Elementresult har string, boolean, osv.. ok og ElementAttribute har string, string.
+
+  // CASE: highlighElement
+  test('handleMessage should highlight a single element', () => {
+  
+    const sendResponseMock = jest.fn();
+  
+    // ElementObject for HighlighMessage
+    const testElement: ElementObject = {
+      title: 'Test Element',
+      htmlString: '<div>Test Element</div>',
+      selector: 'div.test-element',
+      result: {
+        htmlString: '<div>Test Element</div>',
+        issue: false,
+        name: 'Test Element',
+        comment: '',
+        checked: false,
+        url: '',
+        testID: '123',
+      },
+      attributes: [
+        { name: 'attribute1', value: 'value1' },
+        { name: 'attribute2', value: 'value2' },
+      ],
+    };
+
+    // HighlightMessage for handleHighlightSingle
+    const testMessage: HighlightMessage = {
+      action: 'highlightElement',
+      element: testElement,
+      isChecked: false,
+    };
+
+    // Call the handleMessage function
+    handleMessage(testMessage, sender, sendResponseMock);
+
+
+
+  });
+
 });
 
-
+ 
