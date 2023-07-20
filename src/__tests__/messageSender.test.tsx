99e1554aa24f15dcc73dab23c28d9050b3f746d1
AudunOklevik <audun.oklevik@gmail.com>
2023-07-20 10:41:00 +0200

    35 out of 35 tests are passing

--- src/__tests__/messageSender.test.tsx
+++ src/__tests__/messageSender.test.tsx
@@ -131,6 +131,47 @@ describe("MessageSender", () => {
       expect(callback).toHaveBeenCalledWith([]);
     });    
     
+    it("should handle error when sendMessage callback throws an error", () => {
+      const callback = jest.fn();
+    
+      // Mock the chrome.tabs.query function to simulate an active tab
+      const activeTab = { id: 1, url: "example.com" };
+      chrome.tabs.query = jest.fn((queryInfo, queryCallback) => {
+        queryCallback([activeTab]);
+      });
+    
+      // Mock the chrome.tabs.sendMessage function to simulate a callback error
+      chrome.tabs.sendMessage = jest.fn((tabId, message, options, sendMessageCallback) => {
+        try {
+          // Simulate a callback error by throwing an error
+          throw new Error("Simulated error in callback");
+        } catch (error) {
+          // Call the callback with an empty array to handle the error case
+          callback([]);
+        }
+      });
+    
+      // Call the function to test
+      messageSender.scanPageMessage(callback);
+    
+      // Ensure that the chrome.tabs.query function is called
+      expect(chrome.tabs.query).toHaveBeenCalledWith(
+        { active: true, currentWindow: true },
+        expect.any(Function)
+      );
+    
+      // Ensure that the chrome.tabs.sendMessage function is called with the correct arguments
+      expect(chrome.tabs.sendMessage).toHaveBeenCalledWith(
+        activeTab.id,
+        expect.any(ScanPageMessage),
+        expect.any(Function)
+      );
+    
+      // Ensure that the callback is called with an empty array
+      expect(callback).toHaveBeenCalledWith([]);
+    });
+    
+    
 
     it("should send a scan page message to the active tab", () => {
       const callback = jest.fn();
