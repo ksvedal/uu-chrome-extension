f12057173e63b32cee4686d4e272d7f5fa5f9e35
Nitschke <andreas.conradi.nitschke@digdir.no>
2023-07-20 14:09:57 +0200

    added error handling, most notable is on fetchData and logResults, where the user gets a response on what is wrong. Also fixed comment toggle and added notification for updates

--- src/sidebar/resultsHeader.tsx
+++ src/sidebar/resultsHeader.tsx
@@ -1,43 +1,58 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TabIcon from './tabIcon';
 import { ResultsHeaderInterface } from './interfaces';
 import { MyContext } from './resultItemsContext';
 import { TestUtils } from './testUtils';
 
-const ResultsHeader: React.FC<ResultsHeaderInterface> = ({url, isScanned}) => {
-  if(!isScanned){
+const ResultsHeader: React.FC<ResultsHeaderInterface> = ({ url, isScanned }) => {
+  const [error, setError] = useState<string | null>(null);
+
+  if (!isScanned) {
     return null;
   }
-const context = useContext(MyContext);
-    if (context === null) {
-      // handle the case where the context is null
-      return null;
-    }
-    const { elementResults, setElementResults } = context;
+  const context = useContext(MyContext);
+  if (context === null) {
+    console.error('Context is null');
+    return null;
+  }
+  const { elementResults, setElementResults } = context;
 
-    
-    
-    const logResult= () => {
-      for (let result of elementResults){
-        TestUtils.giveIdChromeAndExtensionVersion(result);
-        console.log(result);
-      };
-      console.log("Result: " + elementResults);
+
+
+  const logResult = () => {
+    if (elementResults == null || elementResults.length === 0) {
+      const errorMsg = "No elements are evaluated yet.";
+      console.error(errorMsg);
+      setError(errorMsg);
+      return;
     }
-  
+
+    for (let result of elementResults) {
+      try {
+        TestUtils.giveIdChromeAndExtensionVersion(result);
+      } catch (error) {
+        console.error('Error processing result:', error);
+      }
+      console.log(result);
+    };
+    setError(null);
+    console.log("Result: " + elementResults);
+  }
+
   return (
     <div className='row headline'>
-        <div className={"col-2"}>
-          <TabIcon></TabIcon>
-        </div>
-        <div className={"col-7"}>
-            <p> {url} </p>
-        </div>
-        <div className={"col-3"}>
-            <button className='headline-button float-right' onClick={logResult}>
-                Print results
-            </button>
-        </div>
+      <div className={"col-2"}>
+        <TabIcon></TabIcon>
+      </div>
+      <div className={"col-7"}>
+        <p> {url} </p>
+      </div>
+      <div className={"col-3"}>
+        <button className='headline-button float-right' onClick={logResult}>
+          Print results
+        </button>
+      </div>
+      {error && <div className="col-12 error-message">{error}</div>}
     </div>
   );
 };
