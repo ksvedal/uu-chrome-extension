cde529d4c4d63129c9123944e6ff68e7197fbaba
Brage Navarsete <brage2000@hotmail.com>
2023-07-24 09:16:22 +0200

    fix bug with collapsibleItemTest

--- src/__tests__/collapsibleItem.test.tsx
+++ src/__tests__/collapsibleItem.test.tsx
@@ -1,12 +1,15 @@
+// Import necessary dependencies and libraries
 import React from "react";
 import { render } from "@testing-library/react";
 import { CollapsibleItemType } from "../sidebar/collapsibleItem";
 import '@testing-library/jest-dom/extend-expect';
 
-
+// Describe the test suite for the 'CollapsibleItemType' component
 describe("CollapsibleItemType", () => {
-    it("should render without crashing", () => {
-      const type = {
+  // Define the test case: "should render without crashing"
+  it("should render without crashing", () => {
+    // Prepare the mock data for the 'type' prop
+    const type = {
         name: "Test Type",
         nodes: [
           {
@@ -26,36 +29,41 @@ describe("CollapsibleItemType", () => {
               outcome: "",
             },
             attributes: [],
+            isCommentVisible: false, // Add the isCommentVisible property here
           },
         ],
         selector: "some-selector", // Add a valid selector here
       };
-  
-      console.log("Rendering CollapsibleItemType...");
-      const { container } = render(
-        <CollapsibleItemType
-          type={type}
-          thisElement={null}
-          isAllHighlighted={false}
-          setIsAllHighlighted={() => {}}
-          setCurrentHighlighted={() => {}}
-          parentIndex={0}
-          url=""
-          testID=""
-        />
-      );
-      console.log("CollapsibleItemType rendered successfully!");
-  
-      // You can also log the rendered container to inspect its content
-      expect(container).toBeInTheDocument();
+      
 
-      // You can also use snapshot testing if needed
-      expect(container).toMatchSnapshot();
-    });
+    // Log a message indicating that the component is being rendered
+    console.log("Rendering CollapsibleItemType...");
 
+    // Render the 'CollapsibleItemType' component with the mock props
+    const { container } = render(
+      <CollapsibleItemType
+        type={type}
+        thisElement={null}
+        isAllHighlighted={false}
+        setIsAllHighlighted={() => {}}
+        setCurrentHighlighted={() => {}}
+        parentIndex={0}
+        url=""
+        testID=""
+      />
+    );
+
+    // Log a message indicating that the component was rendered successfully
+    console.log("CollapsibleItemType rendered successfully!");
+
+    // Perform the test's primary assertion: Check if the container is in the document
+    expect(container).toBeInTheDocument();
+
+    // Optionally, you can use snapshot testing to compare the rendered output with a predefined snapshot
+    expect(container).toMatchSnapshot();
+  });
 });
-  
-    // Add more tests for different functionalities if needed
+
 
 
 
